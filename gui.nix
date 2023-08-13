465d22807663d2891d6580efa022fbc4f9c30a57
Nick Wade <me@ncwade.com>
2023-08-13 14:10:39 -0500

    keep tweaking

--- gui.nix
+++ gui.nix
@@ -11,8 +11,15 @@
 
   xdg.configFile."direnv/direnvrc".source = ./config/direnvrc;
 
+  services.mako = {
+    enable = true;
+    defaultTimeout = 4000;
+  };
+
   home.packages = [
     pkgs.kitty
+    pkgs.waybar
+    pkgs.rofi-wayland
   ];
 }
 
